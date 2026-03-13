b452d99984682e28858f7df548ab4d14b18a5209
nicoars1 <nicolas.arsegot.gustavo@gmail.com>
2026-03-13 13:27:35 -0300

    feat: add login and register pages, flask routes and navigations links

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ def home():
 @app.route('/calendario')
 def calendario():
     return render_template('calendario.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
